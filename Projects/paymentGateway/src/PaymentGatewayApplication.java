27fa2a5c297c14ae058082b3dbf2172c98e46b0f
Silly_coder <maityp394@gmail.com>
2025-07-25 22:29:16 +0530

    ...

--- Projects/paymentGateway/src/PaymentGatewayApplication.java
+++ Projects/paymentGateway/src/PaymentGatewayApplication.java
@@ -5,7 +5,7 @@ import model.PaymentRequest;
 
 public class PaymentGatewayApplication {
     public static void main(String[] args) {
-        PaymentRequest req1 = new PaymentRequest("Aditya", "Shubham", 1000.0, "INR");
+        PaymentRequest req1 = new PaymentRequest("Aditya", "Pritam", 1000.0, "INR");
 
         System.out.println("Processing via Paytm");
         System.out.println("------------------------------");
@@ -13,7 +13,7 @@ public class PaymentGatewayApplication {
         System.out.println("Result: " + (res1 ? "SUCCESS" : "FAIL"));
         System.out.println("------------------------------\n");
 
-        PaymentRequest req2 = new PaymentRequest("Shubham", "Aditya", 500.0, "USD");
+        PaymentRequest req2 = new PaymentRequest("Pritam", "Aditya", 500.0, "USD");
 
         System.out.println("Processing via Razorpay");
         System.out.println("------------------------------");
